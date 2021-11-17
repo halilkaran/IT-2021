90740da436f65491f72baa8e322d0d08b19d58b1
Halil <78084765+halilkaran@users.noreply.github.com>
2021-11-17 11:03:42 +0100

    accoutnt py added

---
+++ git/hands-on/account.py
@@ -0,0 +1,7 @@
+print("account.py created")
+print("Master updated account_v1")
+print("Master updated account_v2")
+print("DEV2 update account_py_v1")
+print("DEV2 updated ************")
+print("*************************")
+
